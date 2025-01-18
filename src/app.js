ae44791800dd5e1413a7cbe0e96e50e1726b8f84
Revamth <rvrevamth@gmail.com>
2025-01-18 20:03:09 +0530

    epsiode 7 complete

--- src/app.js
+++ src/app.js
@@ -6,10 +6,57 @@ const User = require("./models/user");
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-    const user = new User(req.body);
-    await user.save();
-    res.send("User added Successfully!");
+  const user = new User(req.body);
+  await user.save();
+  res.send("User added Successfully!");
 });
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const user = await User.find({emailId: userEmail});
+    res.send(user);
+  }
+  catch(err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const user = await User.find({});
+    res.send(user);
+  }
+  catch(err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  }
+  catch(err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({_id: userId}, data);
+    res.send("User Updated successfully");
+  }
+  catch(err) {
+    res.status(400).send("Something went wrong");
+  }
+})
 
 connectDB()
   .then(() => {
--- src/config/database.js
+++ src/config/database.js
@@ -3,16 +3,14 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://rvrevamth1:revamth@devtinder.tag55.mongodb.net/myDatabase", // Add your database name here.
+      "mongodb+srv://rvrevamth1:revamth@devtinder.tag55.mongodb.net/myDatabase",
       {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
       }
     );
     console.log("Database connected successfully.");
   } catch (err) {
     console.error("Database connection error:", err.message);
-    process.exit(1); // Exit the app if the connection fails.
+    process.exit(1);
   }
 };
 
